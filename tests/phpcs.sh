5f21f16f5b3afaec2a5febd23e9bf5667ffd8c58
Viktor Szépe <viktor@szepe.net>
2019-06-10 20:11:27 +0000

    Clean up old test files

--- tests/phpcs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Should be executed from repository root
-cd -P "$(dirname "${BASH_SOURCE[0]}")/../"
-
-vendor/bin/phpcs --config-set installed_paths vendor/wp-coding-standards/wpcs/
-vendor/bin/phpcs ./http-analyzer/
-vendor/bin/phpcs ./core-events/
